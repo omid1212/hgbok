28830b6cf071929f86984a8164d4f67b8bd84448
asmantarik <asmantarik94@gmail.com>
2015-12-01 20:45:42 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {150529749},
+    sudo_users = {172767635},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
